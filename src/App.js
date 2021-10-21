d36195932dbe0240d2b3b1f41f84e1062e8aa483
MissSiriluck <som23625@gmail.com>
2021-10-21 19:04:31 +0700

    Booking confirmation and Confirm payment page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { BrowserRouter,Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import BookingConfirmationPage from "./pages/BookingConfirmationPage";
+import ConfirmInfoPage from "./pages/ConfirmInfoPage";
 import Homepage from "./pages/Homepage";
 import showResult from "./pages/showResult";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/showResult" component={showResult} />
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/showResult' component={showResult} />
+          <Route
+            exact
+            path='/BookingConfirmation'
+            component={BookingConfirmationPage}
+          />
+          <Route exact path='/ConfirmInformation' component={ConfirmInfoPage} />
         </Switch>
       </BrowserRouter>
     </div>
