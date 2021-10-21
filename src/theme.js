f03fe196e5f22b728bcf97c0cc2b24ebbe497166
MissSiriluck <som23625@gmail.com>
2021-10-21 12:00:38 +0700

    add Homepage

--- src/theme.js
+++ src/theme.js
@@ -3,15 +3,15 @@ import { createTheme } from "@material-ui/core/styles";
 const theme = createTheme({
   typography: {
     fontFamily: "'Noto Sans Thai', sans-serif",
-    components: {
-      MuiCssBaseline: {
-        styleOverrides: `
-          @font-face {
-            font-family: 'Noto Sans Thai', sans-serif;
-          }
-        `,
-      },
-    },
+    // components: {
+    //   MuiCssBaseline: {
+    //     styleOverrides: `
+    //       @font-face {
+    //         font-family: 'Noto Sans Thai', sans-serif;
+    //       }
+    //     `,
+    //   },
+    // },
   },
 });
 
