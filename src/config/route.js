6efe4c17c92f4124f6910e6d29651d183c38b0d0
MissSiriluck <som23625@gmail.com>
2021-10-29 08:45:29 +0700

    add route and responsive owner login page 29/10/2021

--- src/config/route.js
+++ src/config/route.js
@@ -9,6 +9,11 @@ import RegisterPage from "../pages/RegisterPage";
 import ShowResultPage from "../pages/ShowResultPage";
 import UserHistoryPage from "../pages/UserHistoryPage";
 import ResidentRegisterPage2Page from "../pages/ResidentRegisterPage2Page";
+import ResidentRegisterPage3Page from "../pages/ResidentRegisterPage3Page";
+import ResidentRegisterPage4Page from "../pages/ResidentRegisterPage4Page";
+import ResidentRegisterPage5Page from "../pages/ResidentRegisterPage5Page";
+import ResidentRegisterPage6Page from "../pages/ResidentRegisterPage6Page";
+import ResidentRegisterPage8Page from "../pages/ResidentRegisterPage8Page";
 
 const route = {
   guest: [
@@ -97,6 +102,26 @@ const route = {
     {
       path: "/residentregisterpage2",
       component: ResidentRegisterPage2Page,
+    },
+    {
+      path: "/residentregisterpage3",
+      component: ResidentRegisterPage3Page,
+    },
+    {
+      path: "/residentregisterpage4",
+      component: ResidentRegisterPage4Page,
+    },
+    {
+      path: "/residentregisterpage5",
+      component: ResidentRegisterPage5Page,
+    },
+    {
+      path: "/residentregisterpage6",
+      component: ResidentRegisterPage6Page,
+    },
+    {
+      path: "/residentregisterpage8",
+      component: ResidentRegisterPage8Page,
     },
     {
       path: "/ownerdetail",
