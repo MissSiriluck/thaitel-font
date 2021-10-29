98e38afa64d76ab6f617917b2c8a0fd21b5e50c3
Poosit <poosit.pims@gmail.com>
2021-10-29 11:13:34 +0700

    move useEffect to HotelCard component

--- src/component/HotelCard.jsx
+++ src/component/HotelCard.jsx
@@ -1,17 +1,33 @@
 import { Grid, Typography } from "@mui/material";
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect } from "react";
 import { ResidentContext } from "../context/ResidentContext";
 import HotelCardList from "./HotelCardList";
 
 function HotelCard() {
   const { residents, setResidents } = useContext(ResidentContext);
 
+  useEffect(() => {
+    const fetchResident = async () => {
+      try {
+        const res = await axios.get("/residents");
+        setResidents(res.data.resident);
+        // console.log(res.data.resident);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchResident();
+  }, []);
+
+  console.log("residents....................", residents.length);
+
   return (
     <Grid container>
       <Grid item>
-        <Typography
-          sx={{ mb: 4 }}
-        >{`ที่พักทั้งหมด ：พบที่พัก 200 แห่ง`}</Typography>
+        <Typography sx={{ mb: 4 }}>
+          ที่พักทั้งหมด ：พบที่พัก {residents.length} แห่ง
+        </Typography>
       </Grid>
       {residents.map((item) => (
         <HotelCardList resident={item} />
