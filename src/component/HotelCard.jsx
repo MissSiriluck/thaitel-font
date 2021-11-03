c4b202abe7907e28639fc9853c9dbadb723151a9
Poosit <poosit.pims@gmail.com>
2021-11-03 16:19:25 +0700

    edit bug 3/11/21 (16.20)

--- src/component/HotelCard.jsx
+++ src/component/HotelCard.jsx
@@ -1,18 +1,38 @@
 import { Grid, Typography } from "@mui/material";
-// import axios from "axios";
-// import { useContext, useEffect } from "react";
-// import { ResidentContext } from "../context/ResidentContext";
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { ResidentContext } from "../context/ResidentContext";
 import HotelCardList from "./HotelCardList";
 
-function HotelCard(props) {
+function HotelCard({ data }) {
+  const { residents, setResidents } = useContext(ResidentContext);
+
+  useEffect(() => {
+    // const fetchResident = async () => {
+    //   try {
+    //     const res = await axios.get("/residents");
+    //     setResidents(res.data.resident);
+    //     // console.log(res.data.resident);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
+    // fetchResident();
+    setResidents(data);
+  }, [setResidents, data]);
+
+  // console.log("residents....................", residents.length);
+
   return (
     <Grid container>
       <Grid item>
         <Typography sx={{ mb: 4 }}>
-          ที่พักทั้งหมด ：พบที่พัก แห่ง
+          ที่พักทั้งหมด ：พบที่พัก {residents.length} แห่ง
         </Typography>
       </Grid>
-      <HotelCardList data={props.data} />
+      {residents.map((item) => (
+        <HotelCardList resident={item} />
+      ))}
     </Grid>
   );
 }
