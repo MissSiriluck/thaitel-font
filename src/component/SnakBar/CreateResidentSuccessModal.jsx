0e6b1ce5924ab9d82e41f7e44d7464e8b45ae144
Prae152990 <noppsoon@gmail.com>
2021-11-07 14:23:04 +0700

    edit snakeBar date:07112021

--- src/component/SnakBar/CreateResidentSuccessModal.jsx
+++ src/component/SnakBar/CreateResidentSuccessModal.jsx
@@ -1,75 +1,75 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import Button from '@mui/material/Button';
+// import Typography from '@mui/material/Typography';
+// import Stack from '@mui/material/Stack';
+// import Snackbar from '@mui/material/Snackbar';
+// import MuiAlert from '@mui/material/Alert';
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-  };
+// const style = {
+//     position: 'absolute',
+//     top: '50%',
+//     left: '50%',
+//     transform: 'translate(-50%, -50%)',
+//     width: 400,
+//     bgcolor: 'background.paper',
+//     border: '2px solid #000',
+//     boxShadow: 24,
+//     p: 4,
+//   };
 
-  const Alert = React.forwardRef(function Alert(props, ref) {
-    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-  });
+//   const Alert = React.forwardRef(function Alert(props, ref) {
+//     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+//   });
 
-function CreateResidentSuccessModal() {
+// function CreateResidentSuccessModal() {
 
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+//     const [open, setOpen] = React.useState(false);
+//     const handleOpen = () => setOpen(true);
+//     const handleClose = () => setOpen(false);
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+//   const handleClick = () => {
+//     setOpen(true);
+//   };
 
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
+//   const handleClose = (event, reason) => {
+//     if (reason === 'clickaway') {
+//       return;
+//     }
 
-    setOpen(false);
-  };
+//     setOpen(false);
+//   };
 
-  const action = (
-    <React.Fragment>
-      <Button color="secondary" size="small" onClick={handleClose}>
-        UNDO
-      </Button>
-      <IconButton
-        size="small"
-        aria-label="close"
-        color="inherit"
-        onClick={handleClose}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </React.Fragment>
-  );
+//   const action = (
+//     <React.Fragment>
+//       <Button color="secondary" size="small" onClick={handleClose}>
+//         UNDO
+//       </Button>
+//       <IconButton
+//         size="small"
+//         aria-label="close"
+//         color="inherit"
+//         onClick={handleClose}
+//       >
+//         <CloseIcon fontSize="small" />
+//       </IconButton>
+//     </React.Fragment>
+//   );
 
-    return (
-        <>
-            <div>
-                <Button onClick={handleClick}>Open simple snackbar</Button>
-                <Snackbar
-                    open={open}
-                    autoHideDuration={6000}
-                    onClose={handleClose}
-                    message="Note archived"
-                    action={action}
-                />
-            </div>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <div>
+//                 <Button onClick={handleClick}>Open simple snackbar</Button>
+//                 <Snackbar
+//                     open={open}
+//                     autoHideDuration={6000}
+//                     onClose={handleClose}
+//                     message="Note archived"
+//                     action={action}
+//                 />
+//             </div>
+//         </>
+//     )
+// }
 
-export default CreateResidentSuccessModal
+// export default CreateResidentSuccessModal
